075e25d02dd5d21386bd66e33cf0dcf241a098a6
Paul Seyfert <pseyfert.mathphys@gmail.com>
2018-10-16 00:25:08 +0200

    importing knn

--- main.go
+++ main.go
@@ -15,28 +15,61 @@ type testdata struct {
 
 type matkdPoint struct {
 	kdtree.Point
-	val mat.Vector
+	val *mat.Vector
 }
 
-func (p *matkdPoint) Dim() int {
-	return p.val.Len()
+func (p matkdPoint) Dim() int {
+	return (*p.val).Len()
 }
-func (p *matkdPoint) GetValue(dim int) float64 {
-	return p.val.AtVec(dim)
+func (p matkdPoint) GetValue(dim int) float64 {
+	return (*p.val).AtVec(dim)
 }
-func (p *matkdPoint) Distance(other matkdPoint) float64 {
+func (p matkdPoint) Distance(other kdtree.Point) float64 {
 	tmp := mat.NewVecDense(p.Dim(), nil)
-	tmp.SubVec(other.val, p.val)
+	for i := 0; i < p.Dim(); i++ {
+		tmp.SetVec(i, other.GetValue(i))
+	}
+	tmp.SubVec(tmp, *p.val)
 	return mat.Norm(tmp, 2)
 }
-func (p *matkdPoint) PlaneDistance(val float64, dim int) float64 {
+func (p matkdPoint) PlaneDistance(val float64, dim int) float64 {
 	tmp := p.GetValue(dim) - val
 	return tmp * tmp
 }
 func upgradeInterface(p mat.Vector) *matkdPoint {
 	retval := &matkdPoint{}
-	retval.val = p
+	retval.val = &p
 	return retval
+}
+func upgradeInterfaces(m *mat.Dense) []kdtree.Point {
+	points := make([]kdtree.Point, 0)
+	_, cols := m.Dims()
+	for i := 0; i < cols; i++ {
+		tmp := upgradeInterface(m.ColView(i))
+		points = append(points, *tmp)
+	}
+	return points
+}
+
+type HT struct {
+	ttree *kdtree.KDTree
+	btree *kdtree.KDTree
+	k     int
+}
+
+func NewHT(k int, tsample, bsample []kdtree.Point) *HT {
+	retval := &HT{ttree: kdtree.NewKDTree(tsample), btree: kdtree.NewKDTree(bsample)}
+	retval.k = k
+	return retval
+}
+func NewHTfromTest(k int, t *testdata) *HT {
+	return NewHT(k, upgradeInterfaces(t.T), upgradeInterfaces(t.B))
+}
+
+func (ht *HT) distt(p *kdtree.Point) float64 {
+	neighbours := ht.ttree.KNN(*p, ht.k)
+	lastneighbour := neighbours[ht.k-1]
+	return (*p).Distance(lastneighbour)
 }
 
 func main() {
@@ -49,13 +82,9 @@ func main() {
 
 	rand.Seed(int64(0))
 	toy, _ := newTestdata(mean_b, cov_b, mean_t, cov_t, 200000)
-	fmt.Printf(" test seed stuff %f\n", toy.T.At(1, 44))
 
-	points_b := make([]matkdPoint, 0)
-	_, cols := toy.B.Dims()
-	for i := 0; i < cols; i++ {
-		points_b = append(points_b, *upgradeInterface(toy.B.ColView(i)))
-	}
+	myht := NewHTfromTest(4, toy)
+	fmt.Printf("using variable %v+\n", myht)
 
 	// plotToy(toy)
 }
